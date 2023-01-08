d8b7c0e09e9bae8996939049f6c970f0b67b7ca2
Sousini <j.asousa2004@gmail.com>
2023-01-08 16:13:14 +0000

    Create ficha6.hs

---
+++ 1ano/programacao-funcional/fichas/ficha6.hs
@@ -0,0 +1,36 @@
+
+
+
+-- Questão 1 
+
+data BTree a = Empty 
+             | Node a (BTree a) (BTree a) 
+          deriving Show 
+
+
+-- a 
+
+altura :: BTree a -> Int 
+altura Empty = 0
+altura (Node _ e d) = 1 + max (altura e) (altura d) 
+
+-- b 
+
+contaNodos :: BTree a -> Int 
+contaNodos Empty = 0 
+contaNodos (Node _ e d) = 1 + contaNodos e + contaNodos d 
+
+-- c 
+
+folhas :: BTree a -> Int 
+folhas Empty = 0 
+folhas (Node _ Empty Empty) = 1 
+folhas (Node _ e d) = folhas e + folhas d 
+
+
+-- d 
+
+prune :: Int -> BTree a -> BTree a 
+prune n Empty = Empty 
+prune 0 _ = Empty 
+prune n (Node x e d) = Node x (prune (n-1) e) (prune (n-1) d) 
