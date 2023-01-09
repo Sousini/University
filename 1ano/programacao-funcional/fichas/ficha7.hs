0b96de4a60b74fe0ee7c4d516b72260b985999c1
Sousini <j.asousa2004@gmail.com>
2023-01-09 00:19:36 +0000

    Create ficha7.hs

---
+++ 1ano/programacao-funcional/fichas/ficha7.hs
@@ -0,0 +1,35 @@
+
+
+-- Questão 1 
+
+data ExpInt = Const Int
+            | Simetrico ExpInt
+            | Mais ExpInt ExpInt
+            | Menos ExpInt ExpInt
+            | Mult ExpInt ExpInt 
+
+
+-- a 
+
+calcula :: ExpInt -> Int 
+calcula (Const x) = x  
+calcula (Simetrico e) = - (calcula e) 
+calcula (Mais e1 e2)  = calcula e1 + calcula e2 
+calcula (Menos e1 e2) = calcula e1 - calcula e2
+calcula (Mult e1 e2)  = calcula e1 * calcula e2 
+
+
+-- b 
+
+infixa :: ExpInt -> String  
+infixa (Const x) = show x  
+infixa (Simetrico e) = "-" ++ infixa e
+infixa (Mais e1 e2)  = "(" ++ infixa e1 ++ "+" ++ infixa e2 ++ ")" 
+infixa (Menos e1 e2) = "(" ++ infixa e1 ++ "-" ++ infixa e2 ++ ")" 
+infixa (Mult e1 e2)  = "(" ++ infixa e1 ++ "*" ++ infixa e2 ++ ")" 
+
+
+--c 
+
+posfixa :: ExpInt -> String 
+posfixa 
