354c538e6b6281929b6c5335323c1fe93175af57
João Sousa <j.asousa2004@gmail.com>
2023-02-23 18:09:53 +0000

    AL

---
+++ 1ano/laboratorios-algoritmia/Mooshak/semana1/A.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>  
+#include <stdlib.h> 
+
+
+#define ve_maior(posicaovalor) {\
+    if (scanf("%d", &valor) != 1) abort ();\
+    if (valor > maior) {\
+        maior = valor;\
+        ordem = posicaovalor;\
+    }\
+}
+
+
+int main() {
+    int maior = 0;
+    int ordem = 0;
+    int valor; 
+
+    ve_maior(1);
+    ve_maior(2);
+    ve_maior(3);
+    ve_maior(4);
+    ve_maior(5); 
+
+    printf("%d\n", ordem); 
+    return 0;
+}
