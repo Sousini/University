b92ecdc515760927133c0a64c24f3a51d44ca773
João Sousa <j.asousa2004@gmail.com>
2023-02-25 20:48:37 +0000

    3.1

--- 1ano/programacao-imperativa/fichas/ficha1.c
+++ 1ano/programacao-imperativa/fichas/ficha1.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
 
 
 
 
 // 3.1
 
+void quadrado(int n) {
+    int l, c; 
+    for (l = 0; l < n; l++){
+        for (c = 0; c < n; c++){
+            putchar('#');
+        }
+        printf("\n");
+    }
+    system("pause");  
+}
+
 int main() {
-    quadrado();
+    quadrado(5);
     return 0;
 }
