096ed9f1dc33e3e444d3e480d32f046ea497cdd1
João Sousa <j.asousa2004@gmail.com>
2023-02-25 21:02:52 +0000

    C

--- 1ano/programacao-imperativa/fichas/ficha1.c
+++ 1ano/programacao-imperativa/fichas/ficha1.c
@@ -20,4 +20,33 @@ void quadrado(int n) {
 int main() {
     quadrado(5);
     return 0;
+} 
+
+
+//3.2 
+
+void xadrez(int n){
+    int i, j, quadrados; 
+    quadrados = n * n; 
+    i = 0; 
+
+    while(i < quadrados) {
+        for(j=0; j < n; j++) {
+            if (i % 2 == 0) {
+                putchar('#'); 
+                i++; 
+            } 
+            else {
+                putchar('_');
+                i++;
+            }
+        }
+        printf("\n");
+    }
+    system("pause");
+}
+
+int main(){
+    xadrez(5);
+    return 0; 
 }
