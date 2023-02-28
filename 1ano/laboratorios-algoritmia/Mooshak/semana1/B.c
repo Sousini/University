3ddbcc77fa86966ef119913e0534b3b539fb55c7
João Sousa <j.asousa2004@gmail.com>
2023-02-28 13:36:27 +0000

    Update B.c

--- 1ano/laboratorios-algoritmia/Mooshak/semana1/B.c
+++ 1ano/laboratorios-algoritmia/Mooshak/semana1/B.c
@@ -1,3 +1,30 @@
 #include <stdio.h> 
-#include <stlib.h> 
+#include <stdlib.h>
 
+
+int recebe() {
+    int soldado; 
+    if (scanf("%d", &soldado) != 1){
+        abort();
+    } else {
+        return soldado;
+    }
+}
+
+int main() {
+    int s1, s2, s3;
+
+    s1 = recebe();
+    s2 = recebe();
+    s3 = recebe();
+
+
+    if ((s1 == s2 && s2 == s3) || (s1 >= s2 && s2 >= s3 ) || (s1 <= s2 && s2 <= s3)) {
+        printf("OK\n");    
+    } else {
+        printf("NAO\n");
+    }
+
+
+    return 0;
+}
